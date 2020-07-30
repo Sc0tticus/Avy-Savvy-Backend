4ad193d261347c9cbd696bd322df292d2ed2f600
github username <github email address>
2020-07-29 21:46:39 -0600

    updating backend

---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+ActiveRecord::Schema.define(version: 2020_07_30_033140) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "description"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "description"
+    t.string "imageLink"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all 
+Comment.destroy_all
+Post.destroy_all
+
+chris = User.create(username: "chris123", password_digest: "chris123")
+
+post1 = Post.create(description: "Minions", imageLink: "https://ewscripps.brightspotcdn.com/dims4/default/717f971/2147483647/strip/true/crop/720x405+0+264/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F4d%2Fb0%2F542e38e14cb2ac12092fdab68aa1%2Favalanche-of-ten-mile-canyon.jpg",  user: chris)
+
+Comment.create(description: "WOW", post: post1 )
+Comment.create(description: "DUDE", post: post1 )
+Comment.create(description: "Hell yeah", post: post1 )
